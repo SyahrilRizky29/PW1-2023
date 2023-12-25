1b2268844a9d19a04e4f82063c6b447f3a6798a9
SyahrilRizky29 <syahrilrizky599@gmail.com>
2023-12-25 21:10:22 +0700

    Tugas JS

---
+++ tugas pre uas/form login/auth.js
@@ -0,0 +1,16 @@
+// let buttonSubmit = document.getElementById("login");
+
+// buttonSubmit.onclick = function(){
+//     let inputNama = document.getElementById('username').value;
+//     let inputPassword = document.getElementById('password').value;
+//     let form = document.getElementById('form');
+
+//     if(inputNama == 'ariel' && inputPassword == 'qwerty'){
+//         alert('Login Berhasil');
+//         form.setAttribute('action', 'dashboard.html');
+//     } 
+//     else {
+//         alert('username atau password salah')
+//     }
+    
+// }
